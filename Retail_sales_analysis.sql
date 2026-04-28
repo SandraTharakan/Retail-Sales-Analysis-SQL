87fa46b8def658a9983ce875772e5c5c66c487bc
SandraTharakan <sandraptharakan@gmail.com>
2026-04-28 18:13:02 +0530

    Updated SQLfile

--- Retail_sales_analysis.sql
+++ Retail_sales_analysis.sql
@@ -1,4 +1,4 @@
-
+/*
 CREATE TABLE retail_sales
 (
 transactions_id INT,
@@ -7,24 +7,27 @@ sale_time TIME,
 customer_id INT,
 gender VARCHAR(10),
 age INT,
-category VARCHAR(15),
+category VARCHAR(10),
 quantiy INT,
 price_per_unit FLOAT,
 cogs FLOAT,
 total_sale FLOAT
 );
-
-
+*/
+/*
 ALTER TABLE retail_sales
 ADD PRIMARY KEY(transactions_id);
 
+ALTER TABLE  retail_sales
+ALTER category TYPE VARCHAR(15);
+*/
 
 SELECT * FROM  retail_sales
 LIMIT 50;
 
+SELECT COUNT(*) FROM retail_sales;
 
-
---DATA CLEANING (NULL VALUE CHECK)
+--Data cleaning(NULL VALUE CHECK)---
 SELECT COUNT(*) FROM retail_sales
 WHERE 
 	transactions_id	IS NULL
@@ -48,12 +51,10 @@ WHERE
 	cogs	IS NULL
 	OR
 	total_sale	IS NULL
-	
 
 --DATA EXPLORATION-
 --Q] How many sales we have ?
 SELECT COUNT(total_sale) FROM retail_sales
---OR
 SELECT COUNT(*) as total_sale FROM retail_sales
 
 --Q] How many unique customers are there?
@@ -65,29 +66,27 @@ SELECT DISTINCT Category FROM retail_sales
 --Q] Date Range of Data
 SELECT MIN(sale_date), MAX(sale_date) FROM retail_sales
 
-
-
 --Data Analysis and Business key problems
 
 --1. Retrieve all transactions made on '2022-11-05'
 --2. Retrieve transaction details where category is 'Clothing' and quantity ≥ 4 in Nov 2022
 --3. Calculate the total sales for each category.
---4. Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
---5. Write a SQL query to find all transactions where the total_sale is greater than 1000.
---6. Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
---7. Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
---8. Write a SQL query to find the top 5 customers based on the highest total sales 
---9. Write a SQL query to find the number of unique customers who purchased items from each category.
---10. Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
---11. Write a SQL Query to calculate Gross Profit and Profit Margin by Category
---12. Write a SQL Query to Find highest Category by Profit in Each Year (Most profitable category per year)
---13. Write a SQL Query to Find the Monthly Revenue Growth Rate (%)
---14. Write a SQL Query to Identify Repeat Customers(low, medium, high) vs One-time Customers
---15. Write a SQL Query to Calculate Customer Lifetime Value (CLV)
---16. Write a SQL Query to Detect Customers with Increasing Spending Trend (Monthly)
---17. Write a SQL Query to Find Peak Sales Hour of the Day
--- 18. Write a SQL Query to Category Contribution % to Total Revenue ([category revenue/total revenue] *100)
+--4. Find the average age of customers who purchased items from the 'Beauty' category.
+--5. Find all transactions where the total_sale is greater than 1000.
+--6. Find the total number of transactions (transaction_id) made by each gender in each category.
+--7. Calculate the average sale for each month. Find out best selling month in each year
+--8. Find the top 5 customers based on the highest total sales 
+--9. Find the number of unique customers who purchased items from each category.
+--10. Categorize transactions into shifts (Morning, Afternoon, Evening) and count orders
+--11. Calculate Gross Profit and Profit Margin by Category
+--12. Find highest Category by Profit in Each Year (Most profitable category per year)
+--14. Classify customers as one-time, low, medium, high repeat customers
+--15. Calculate Customer Lifetime Value (CLV)
+--16. Detect Customers with Increasing Spending Trend (Monthly)
+--17. Find Peak Sales Hour of the Day
+-- 18. Calculate Category Contribution % to Total Revenue ([category revenue/total revenue] *100)
 -- 19. Cohort Retention Analysis
+
 
 
 
@@ -117,10 +116,10 @@ SELECT
 	COUNT(*) AS Total_orders 
 FROM retail_sales
 GROUP BY category
---INSIGHT: Electronics generates the highest revenue, indicating it is the top-performing product category.
+--INSIGHT: Electronic products generates the highest revenue, indicating it is the top-performing product category.
 
 
---4. Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+--4. Find the average age of customers who purchased items from the 'Beauty' category.
 SELECT 
 	category,
 	ROUND(AVG(age),2) AS Avg_cust_Age,
@@ -128,14 +127,15 @@ SELECT
 FROM retail_sales
 WHERE Category='Beauty'
 GROUP BY category
+--INSIGHT: Beauty category shows higher engagement among middle-aged customers.
 
 
---5. Write a SQL query to find all transactions where the total_sale is greater than 1000.
+--5. Find all transactions where the total_sale is greater than 1000.
 SELECT * FROM retail_sales
 WHERE total_sale>1000
 
 
---6. Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+--6. Find the total number of transactions (transaction_id) made by each gender in each category.
 SELECT
 	Category,
 	gender,
@@ -145,9 +145,10 @@ GROUP BY
     category,
     gender
 ORDER BY 1
+--INSIGHT: Female customers dominate transactions in the Beauty category, while gender-based transaction differences are minimal in other categories.
 
 
---7. Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+--7. Calculate the average sale for each month. Find out best selling month in each year
 WITH TABLE2 AS
 (
 SELECT 
@@ -159,25 +160,29 @@ SELECT
 FROM retail_sales
 GROUP BY 1,2
 )
+
 SELECT * FROM TABLE2 WHERE rank=1
+--INSIGHT: The average monthly sales range between 350 and 550, with July 2022 and February 2023 emerging as the peak months in their respective year.
 
 
---8. Write a SQL query to find the top 5 customers based on the highest total sales 
+--8. Find the top 5 customers based on the highest total sales 
 SELECT customer_id, SUM(total_sale)
 FROM retail_sales
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 5
+--INSIGHT: Sales are relatively well distributed across customers, with no heavy reliance on a small group of top customers.
 
 
---9. Write a SQL query to find the number of unique customers who purchased items from each category.
+--9. Find the number of unique customers who purchased items from each category.
 SELECT category, COUNT(DISTINCT customer_id) AS Cust_count
 FROM retail_Sales
 GROUP BY 1
 ORDER BY 1
+--INSIGHT: Customer distribution is fairly uniform across categories, indicating balanced customer interest across product segments.
 
 
---10. Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
+--10.Categorize transactions into shifts (Morning, Afternoon, Evening) and count orders
 SELECT COUNT(*) AS NumOfOrders,
 CASE
 	WHEN EXTRACT(HOUR FROM sale_time )<12 THEN 'Morning'
@@ -186,9 +191,10 @@ CASE
 END AS Hour_sale
 FROM retail_sales
 GROUP BY Hour_sale
+-- INSIGHT: A significant portion of transactions occurs during the evening hours, making it the peak sales period of the day.
 
 
---11. Write a SQL Query to calculate Gross Profit and Profit Margin by Category
+--11. Calculate Gross Profit and Profit Margin by Category
 SELECT 
 	category, 
 	SUM(total_sale) AS Total_revenue, 
@@ -197,10 +203,11 @@ SELECT
 	ROUND((SUM(total_sale-cogs)/SUM(total_sale))::numeric,2) *100 AS Profit_Margin
 	FROM retail_sales
 GROUP BY 1
+--INSIGHT: All categories exhibit similar profit margins (~79–80%), suggesting uniform profitability and a consistent cost-to-revenue ratio across products.
 
 
 --12. Find highest Category by Profit in Each Year (Most profitable category per year)
-WITH Table3 AS(
+WITH Category_profit AS(
 SELECT 
 	TO_CHAR(sale_date,'YYYY') AS Sale_Year,
 	Category,
@@ -210,14 +217,15 @@ SELECT
 	RANK() OVER 
 		(
 		PARTITION BY TO_CHAR(sale_date,'YYYY') 
-		ORDER BY ROUND(SUM(total_sale-cogs)::numeric,2)
+		ORDER BY ROUND(SUM(total_sale-cogs)::numeric,2) DESC
 		) AS Profit_rank
 FROM retail_sales
 GROUP BY 1,2
 )
 
-SELECT * FROM Table3
+SELECT * FROM Category_profit
 WHERE profit_rank=1
+-- INSIGHT: The most profitable category shifts from Electronics in 2022 to Clothing in 2023, indicating a change in customer demand or product performance over time.
 
 
 --13. Find the Monthly Revenue Growth Rate (%)
@@ -234,32 +242,36 @@ SELECT
 	)::numeric , 2 ) AS Growth_Rate
 FROM retail_sales
 GROUP BY 1
+-- INSIGHT: Monthly revenue growth rate shows fluctuations across the period, with noticeable sharp revenue growth in September 2022 and September 2023, indicating recurring seasonal surges in sales.
 
 
---14. Identify Repeat Customers(low, medium, high) vs One-time Customers
+--14. Classify customers as one-time, low, medium, high repeat customers
 SELECT 
 	Customer_id,
 	COUNT(Customer_id) AS customer_count,
 	CASE
-		WHEN COUNT(Customer_id)=1 THEN '1One-Timer'
-		WHEN COUNT(Customer_id)>1 and COUNT(Customer_id)<11 THEN '2Low-Repeater'
-		WHEN COUNT(Customer_id)>10 and COUNT(Customer_id)<21 THEN '3Med-Repeater'
-		ELSE '4High-Repeater'
+		WHEN COUNT(Customer_id)=1 THEN 'One-Timer'
+		WHEN COUNT(Customer_id)>1 and COUNT(Customer_id)<11 THEN 'Low-Repeater'
+		WHEN COUNT(Customer_id)>10 and COUNT(Customer_id)<21 THEN 'Med-Repeater'
+		ELSE 'High-Repeater'
 	END AS Customer_freq
 FROM retail_sales
 GROUP BY 1
-ORDER BY Customer_freq
-
+ORDER BY customer_count
+/* INSIGHT: All customers are repeat buyers, with no one-time customers observed, indicating strong customer retention. 
+			Some customers exhibit high engagement, with purchase frequencies reaching up to 76 transactions. */
 
 -- 15. Calculate Customer Lifetime Value (CLV)
 SELECT 
 	Customer_id,
 	COUNT(transactions_id) AS total_transactions,
 	SUM(total_sale)AS total_purchaseAmt_CLV,
-	ROUND( (SUM(total_sale)/ COUNT(transactions_id))::numeric ,2) AS avg_orderValue,
-	RANK() OVER (ORDER BY (SUM(total_sale)) DESC) AS rank
+	ROUND( (SUM(total_sale)/ COUNT(transactions_id))::numeric ,2) AS avg_orderValue
+	--RANK() OVER (ORDER BY (SUM(total_sale)) DESC) AS rank
 FROM retail_sales
 GROUP BY 1
+ORDER BY 4 DESC
+-- INSIGHT: Customer lifetime value is relatively well distributed across customers
 
 
 -- 16. Detect Customers with Increasing Spending Trend (Monthly)
@@ -274,6 +286,8 @@ SELECT
 FROM retail_sales
 GROUP BY 1,2
 ORDER BY 1
+/* INSIGHT: Customer spending shows an overall increasing trend, with no negative growth observed, 
+			indicating steadily rising customer value over time despite minor fluctuations. */
 
 
 --17. Find Peak Sales Hour of the Day
@@ -302,16 +316,18 @@ SELECT
 FROM Peak_Hour
 GROUP BY 1
 ORDER BY Freq_at_Hours DESC
+-- INSIGHT: The 7 PM to 8 PM time slot generates the highest revenue across days, indicating peak sales activity during evening hours.
 
 
--- 18. Write a SQL Query to Category Contribution % to Total Revenue ([category revenue/total revenue] *100)
+-- 18. Calculate Category Contribution % to Total Revenue ([category revenue/total revenue] *100)
 SELECT 
 	category,
 	SUM(total_sale) AS Category_revenue,
-	SUM(SUM(total_sale)) OVER() AS net,
+	SUM(SUM(total_sale)) OVER() AS total_revenue,
 	ROUND(((SUM(total_sale)/ SUM(SUM(total_sale)) OVER())*100)::numeric,2) AS Category_Contribution
 FROM retail_sales
 GROUP BY 1
+-- INSIGHT: Revenue contribution is fairly evenly distributed across categories, indicating a balanced dependence on different product segments.
 
 
 -- 19. Cohort Retention Analysis
@@ -342,6 +358,7 @@ INNER JOIN Activity_data a
 ON c.customer_id=a.customer_id
 ),
 
+
 --difference ([Active_Year- Cohort_Year]*12)+(Active_month - Cohort_month)
 CALC_Month_Index AS(
 SELECT
@@ -356,10 +373,12 @@ FROM Cohort_Activity_data
 SELECT 
 	cohort_month,
 	Month_index,
-	COUNT(DISTINCT customer_id)
+	COUNT(DISTINCT customer_id) AS No_of_cutomers
 FROM CALC_Month_Index
 GROUP BY 1,2
 ORDER BY 1,2
+
+-- INSIGHT: Customer retention gradually decreases over subsequent months, indicating diminishing engagement over time.
 
 
 
